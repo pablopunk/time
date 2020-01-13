b5eba993cc78a5a3c53ca543450375433c86c927
pablopunk <pablovarela182@gmail.com>
2020-01-13 17:31:17 +0100

    Refactor using nextjs. Fix #1
    - Changed default font
    - Add fontSize option
    - Don't use # in colors
    - Changed the custom font example
    - Link Plash in readme
    - Change example URL
    - Add randomColors option with nice-color-palettes
    - Link to repo with mouse interaction

---
+++ pages/index.tsx
@@ -0,0 +1,147 @@
+import React from "react";
+import * as validHexColor from "valid-hex-color";
+import palettes from "nice-color-palettes";
+
+function whatTimeIsIt(seconds: boolean) {
+  let time = new Date().toString().split(" ")[4];
+  if (!seconds) {
+    time = time.replace(/(\d+:\d+):\d+/, "$1");
+  }
+
+  return time;
+}
+
+interface IState {
+  time: string;
+  mouseInteraction: boolean;
+}
+
+interface IProps {
+  seconds: boolean;
+  randomColors: boolean;
+  fg: string;
+  bg: string;
+  font: string;
+  fontSize: string;
+  showLink: boolean;
+}
+
+function normalizeColors(colors) {
+  const normalized = {};
+  ["fg", "bg"].map(key => {
+    if (colors[key] != null) {
+      if (validHexColor.check(`#${colors[key]}`)) {
+        normalized[key] = `#${colors[key]}`;
+      } else {
+        normalized[key] = colors[key];
+      }
+    }
+  });
+
+  return {
+    ...colors,
+    ...normalized
+  };
+}
+
+function randomizeColors(colors) {
+  const palette = palettes[Math.ceil(Math.random() * palettes.length)];
+
+  return {
+    ...colors,
+    fg: palette[0],
+    bg: palette[palette.length - 1]
+  };
+}
+
+export default class extends React.Component<IProps, IState> {
+  static async getInitialProps({ query }) {
+    query = normalizeColors(query);
+
+    if (query.randomColors != null) {
+      query = randomizeColors(query);
+    }
+
+    return {
+      font: `system-ui,
+              -apple-system,
+              'Segoe UI',
+              Roboto,
+              Helvetica,
+              Arial,
+              sans-serif,
+              'Apple Color Emoji',
+              'Segoe UI Emoji'`,
+      bg: "black",
+      fg: "royalblue",
+      fontSize: "10em",
+      ...query,
+      seconds: query.seconds != null,
+      randomColors: query.randomColors != null,
+      showLink: query.showLink != null
+    };
+  }
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      time: whatTimeIsIt(props.seconds),
+      mouseInteraction:
+        this.props.showLink == null ? false : this.props.showLink
+    };
+  }
+
+  componentDidMount() {
+    setInterval(() => {
+      this.setState({ time: whatTimeIsIt(this.props.seconds) });
+    }, 1000);
+  }
+
+  render() {
+    return (
+      <>
+        {this.state.mouseInteraction && (
+          <a href="https://github.com/pablopunk/time">Code available here</a>
+        )}
+        <main onMouseOver={() => this.setState({ mouseInteraction: true })}>
+          <div id="time">{this.state.time}</div>
+          <style global jsx>{`
+            body,
+            html {
+              margin: 0;
+              padding: 0;
+              width: 100vw;
+              height: 100vh;
+            }
+            main {
+              width: 100vw;
+              height: 100vh;
+              display: flex;
+              align-items: center;
+              justify-content: center;
+              font-weight: bold;
+              color: ${this.props.fg};
+              background-color: ${this.props.bg};
+            }
+            #time {
+              font-family: ${this.props.font};
+              font-size: ${this.props.fontSize};
+            }
+            a {
+              position: absolute;
+              top: 20px;
+              left: 20px;
+              font-size: 1.2rem;
+              font-family: system-ui, -apple-system, "Segoe UI", Roboto,
+                Helvetica, Arial, sans-serif, "Apple Color Emoji",
+                "Segoe UI Emoji";
+              color: ${this.props.fg};
+              opacity: 0.7;
+            }
+          `}</style>
+        </main>
+      </>
+    );
+  }
+}
--- readme.md
+++ readme.md
@@ -2,18 +2,26 @@
 
 > https://time.pablopunk.now.sh
 
+Perfect if you use tools like [Plash](https://sindresorhus.com/plash).
+
 ## Customize options
 
 - `fg`: Font color. Examples:
   - `?fg=red`
-  - `?fg=#ff00ff`
+  - `?fg=ff00ff`
 - `bg`: Background color.
 - `font`: Font family. Examples:
-  - `?font=monospace`
+  - `?font=SFMono-Regular,Consolas,'Liberation Mono',Menlo,monospace`
+- `fontSize`: The size of the font in the clock. Examples:
+  - `?fontSize=5rem`
+  - `?fontSize=200px`
 - `seconds`: Show seconds if this argument present.
+- `randomColors`: Sets a random color both for background and foreground.
+- `showLink`: A link to this repo is shown automatically when mouse input is detected, but it can be forced using this option.
 
 ## Examples
 
-- https://time.pablopunk.now.sh?seconds&fg=green&bg=royalblue&font=monospace
-- https://time.pablopunk.now.sh?fg=#11cc88&bg=#454545&font=monospace
-- https://time.pablopunk.now.sh?seconds
+- time.pablopunk.now.sh/?seconds&fg=F1396D&bg=382F32&font=monospace
+- https://time.pablopunk.now.sh?fg=11cc88&bg=454545&font=monospace&fontSize=200px
+- https://time.pablopunk.now.sh?seconds&randomColors
+
