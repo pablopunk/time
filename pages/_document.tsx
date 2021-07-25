770432b1bbdb5cfd7819d1a125e095b7d89d9c1c
Pablo P Varela <pablo@pablopunk.com>
2021-07-25 11:56:13 +0200

    fix build

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,15 +1,17 @@
-import { Main, NextScript, Html, Head } from 'next/document'
+import Document, { Main, NextScript, Html, Head } from 'next/document'
 
-export default function MyDocument() {
-  return (
-    <Html>
-      <Head>
-        <script data-goatcounter="/goat" async src="/count.js"></script>
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <script data-goatcounter="/goat" async src="/count.js"></script>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
 }
