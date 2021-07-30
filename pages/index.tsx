38f705cef74be2c48eece4dd7d4a23c7967c358c
Pablo P Varela <pablo@pablopunk.com>
2021-07-30 16:17:18 +0200

    fix for Plash

--- pages/index.tsx
+++ pages/index.tsx
@@ -223,6 +223,7 @@ export default class extends React.Component<IProps, IState> {
               padding: 0;
               width: 100vw;
               height: 100vh;
+              overflow: hidden;
             }
             main {
               width: 100vw;
