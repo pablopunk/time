fc6b027230e3d78fbb1623fe89a39bf6fb0135e6
Pablo P Varela <pablo@pablopunk.com>
2026-07-05 16:10:05 +0200

    fix: remove outputFileTracingRoot causing Vercel deploy failure

--- next.config.js
+++ next.config.js
@@ -1,10 +1,4 @@
-const path = require('path')
-
 module.exports = {
-  outputFileTracingRoot: path.join(__dirname, '..'),
-  turbopack: {
-    root: path.join(__dirname, '..'),
-  },
   async rewrites() {
     return [
       {
